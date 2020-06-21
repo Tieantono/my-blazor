e5cbb08a9137daac3dd0f4ac1288a0fedda02b33
Tie Antono <tietieantono@gmail.com>
2020-06-21 15:02:36 +0700

    Fixed TabNav component Moved TabNav exmaple to another page

--- MyBlazor/Shared/Tabs/TabItem.cs
+++ MyBlazor/Shared/Tabs/TabItem.cs
@@ -1,14 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace MyBlazor.Shared.Tabs
+﻿namespace MyBlazor.Shared.Tabs
 {
+    /// <summary>
+    /// Tab item model class.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class TabItem<T>
     {
+        /// <summary>
+        /// Gets or sets the tab title
+        /// </summary>
         public string Title { get; set; }
 
+        /// <summary>
+        /// Gets or sets the tab item based on the generic parameter <seealso cref="T"/> type.
+        /// </summary>
         public T Item { get; set; }
     }
 }
